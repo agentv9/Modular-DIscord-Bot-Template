0846d87d30b56076d5359e6f2d84ad638a422615
kaseknife95 <agentawesome323@gmail.com>
2024-11-26 20:48:55 -0600

    CLI branch setup

--- TODO.md
+++
@@ -1,3 +0,0 @@
- [] Rewrite template in TypeScript.
- [] Premade modules - Moderation, Fun, Music?, XP, Etc.
- [] Possible command line utility to template out bots.
--- src/util/index.js
+++
@@ -1,5 +0,0 @@
-// - Database exports
-module.exports.sql = require("./db/sequelize")
-
-module.exports.configHandler = require("./handlers/configHandler")
-module.exports.translationHandler = require("./handlers/translationHandler")
